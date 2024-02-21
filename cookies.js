150df937f9b4d8da5fd6809fac819ae9fca3a00a
maleficarius <maesito23@hotmail.com>
2024-02-20 22:28:28 -0600

    Add dropdown and cookies functionality

---
+++ cookies.js
@@ -0,0 +1,25 @@
+
+function setCookies(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    const sameSite = "SameSite=Lax";  // Puedes ajustar esto según tus necesidades
+    document.cookie = name + "=" + value + ";" + expires + ";path=/;" + sameSite;
+}
+
+
+function getCookies(name) {
+    const cookieName = name + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(';');
+    for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(cookieName) === 0) {
+            return cookie.substring(cookieName.length, cookie.length);
+        }
+    }
+    return "";
+}

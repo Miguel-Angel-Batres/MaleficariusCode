9a9f208d5ae0f598d7f5ac8339664d6fa5c551a3
maleficarius <maesito23@hotmail.com>
2024-02-21 00:12:59 -0600

    Refactor cookie handling and add language toggle button

--- cookies.js
+++ cookies.js
@@ -1,10 +1,10 @@
 
 function setCookies(name, value, days) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    const expires = "expires=" + date.toUTCString();
-    const sameSite = "SameSite=Lax";  // Puedes ajustar esto según tus necesidades
-    document.cookie = name + "=" + value + ";" + expires + ";path=/;" + sameSite;
+    const expires = new Date();
+    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+    const sameSite = ';SameSite=None';
+    const secure = ';Secure'; /*because of HTTPS*/
+    document.cookie = name + '=' + value  + ';expires=' + expires.toUTCString() + ';path=/' + sameSite + secure;
 }
 
 
@@ -22,4 +22,71 @@ function getCookies(name) {
         }
     }
     return "";
+}
+
+function cookieExists(name) {
+    if (getCookies(name) !== null) {
+        return true;
+    }
+    return false;
+}
+function switchLanguage(lang) {
+    let path = fetchCurrentPath();
+    setCookies('language', lang, 30);
+    let newPath;
+
+    if (path.endsWith('/') === true) {
+        if (lang === 'es') {
+            newPath = path + 'es';
+        } else {
+            newPath = path.replace('es/', '');
+        }
+    } else if (path.endsWith('index.html') === true) { 
+        if (lang === 'es') {
+            newPath = path.replace('/index.html', '/es/index.html');
+        } else {
+            newPath = path.replace('/es/index.html', '/index.html');
+        }
+    }
+    if(newPath !== undefined){
+        window.location.href = newPath;
+    }
+}
+
+function oppositeLanguage(lang) {
+    if (lang === 'en') {
+        return 'es';
+    }
+    return 'en';
+}
+
+function toggleLanguage(button) {
+    button.disabled = true;
+    let currentLang;
+    if ( cookieExists('language') ) {
+        currentLang = getCookies('language');
+    } else { 
+        currentLang = fetchCurrentSiteLanguage();
+    }
+
+    switchLanguage(oppositeLanguage(currentLang));    
+    setTimeout(function() {
+        button.disabled = false;
+    }, 500);
+}
+
+function fetchCurrentPath(){
+    return window.location.pathname;
+}
+
+function fetchCurrentSiteLanguage(){
+    let currentPath = fetchCurrentPath();
+    if(currentPath.endsWith('/es/') || currentPath.endsWith('/es/index.html')){
+        return 'es';
+    }
+    return 'en';
+}
+
+function getDefaultLanguage(){
+    return navigator.language.substring(0, 2);
 }
